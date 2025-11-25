67082cf107b6d8e2fe773409b3f54d9b068850e6
cantti <cantti@outlook.com>
2025-11-25 22:09:05 +0100

    demo.gif update

---
+++ demo/update-demo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+dir="$PWD"
+TMPDIR=$(mktemp -d)
+
+# Publish to temp directory to avoid polluting cwd
+dotnet publish -c Release --self-contained true \
+  -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true \
+  -p:DebugType=None -p:DebugSymbols=false \
+  -o "$TMPDIR" ../TagSelecta.Cli
+
+# Copy additional assets
+cp song.mp3 "$TMPDIR"
+cp demo.tape "$TMPDIR"
+
+cd "$TMPDIR"
+
+# Generate animated terminal demo
+vhs demo.tape
+
+# Move resulting GIF back to original directory
+mv demo.gif "$dir/"
+
+cd "$dir"
+rm -rf "$TMPDIR"
