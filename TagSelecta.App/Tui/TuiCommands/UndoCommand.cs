42c5671eafefb45957eca541644dc72e67708736
cantti <cantti@outlook.com>
2026-01-10 23:33:11 +0100

    fix: update UndoCommand to use SelectedOperations instead of Operations in TuiCommands

--- TagSelecta.App/Tui/TuiCommands/UndoCommand.cs
+++ TagSelecta.App/Tui/TuiCommands/UndoCommand.cs
@@ -5,7 +5,7 @@ public class UndoCommand : ITuiCommand
 {
     public Task ExecuteAsync(ITuiCommandContext context, Request request, CancellationToken token)
     {
-        foreach (var operation in context.Operations.Where(x => x.HasChanges).ToList())
+        foreach (var operation in context.SelectedOperations.Where(x => x.HasChanges).ToList())
         {
             operation.Undo();
         }
