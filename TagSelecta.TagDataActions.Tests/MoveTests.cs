8c18a1fc1e1d4ff3e31a38df0374b7bd18db0b3c
cantti <cantti@outlook.com>
2026-01-17 17:24:05 +0100

    test: more test

--- TagSelecta.TagDataActions.Tests/MoveTests.cs
+++ TagSelecta.TagDataActions.Tests/MoveTests.cs
@@ -27,4 +27,26 @@ public class MoveTests
         // Assert
         Assert.Equal("/1990 - Test Album/file.mp3", item.CurrentPath);
     }
+    
+    [Fact]
+    public async Task MoveTest_Relative()
+    {
+        // Arrange
+        var action = new MoveAction();
+
+        var settings = new MoveSettings
+        {
+            Template = "../{{ year }} - {{album}}/{{filename}}"
+        };
+
+        var tagData = new TagData() { Date = "1990", Album = "Test Album"};
+
+        var item = new TagDataOperation("/dir/file.mp3", tagData);
+
+        // Act
+        await action.ExecuteAsync(item, [item], settings, CancellationToken.None);
+
+        // Assert
+        Assert.Equal("/1990 - Test Album/file.mp3", item.CurrentPath);
+    }
 }
