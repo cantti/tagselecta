3d9320d4a254b8f80bf2e16096ade927d0463f42
cantti <cantti@outlook.com>
2026-02-09 09:24:35 +0100

    simplify command prompt

--- TagSelecta.Commands/Tui/Widgets/CommandPromptWidget.cs
+++ TagSelecta.Commands/Tui/Widgets/CommandPromptWidget.cs
@@ -8,7 +8,7 @@ public class CommandPromptWidget(string text, int cursorPos) : Renderable
     protected override IEnumerable<Segment> Render(RenderOptions options, int maxWidth)
     {
         var cols = new List<IRenderable>();
-        cols.Add(new Text(":", GetStyle(TokenKind.Command)));
+        cols.Add(new Text(":"));
 
         foreach (var token in Tokenize(text))
         {
@@ -59,11 +59,6 @@ public class CommandPromptWidget(string text, int cursorPos) : Renderable
     {
         return kind switch
         {
-            TokenKind.Whitespace => Style.Plain,
-            TokenKind.AndAnd => new Style(Color.Grey, decoration: Decoration.Bold),
-            TokenKind.Command => new Style(Color.Default, decoration: Decoration.Bold),
-            TokenKind.Key => new Style(Color.Blue),
-            TokenKind.Equals => new Style(Color.Default),
             TokenKind.Value => new Style(Color.Yellow),
             TokenKind.QuotedValue => new Style(Color.Yellow),
             TokenKind.Cursor => new Style(decoration: Decoration.Invert),
@@ -71,59 +66,29 @@ public class CommandPromptWidget(string text, int cursorPos) : Renderable
         };
     }
 
-    private static bool IsIdentPart(char c)
-    {
-        return char.IsLetterOrDigit(c) || c == '_' || c == '-';
-    }
-
     private IEnumerable<Token> Tokenize(string input)
     {
         var i = 0;
 
-        var expectingCommand = true;
-        var expectingKeyOrFlag = false;
-
         while (i < input.Length)
         {
-            var c = input[i];
-
-            if (char.IsWhiteSpace(c))
+            // value in ""
+            if (input[i] == '"')
             {
                 var start = i;
-                while (i < input.Length && char.IsWhiteSpace(input[i]))
-                {
-                    i++;
-                }
-
-                yield return new Token(start, i - start, TokenKind.Whitespace);
-                continue;
-            }
-
-            if (c == '&' && i + 1 < input.Length && input[i + 1] == '&')
-            {
-                yield return new Token(i, 2, TokenKind.AndAnd);
-                i += 2;
-                expectingCommand = true;
-                expectingKeyOrFlag = false;
-                continue;
-            }
-
-            if (c == '"')
-            {
-                var start = i;
-                i++;
+                i++; // opening "
 
                 while (i < input.Length)
                 {
                     if (input[i] == '\\' && i + 1 < input.Length)
                     {
-                        i += 2;
+                        i += 2; // skip escaped char
                         continue;
                     }
 
                     if (input[i] == '"')
                     {
-                        i++;
+                        i++; // closing ""
                         break;
                     }
 
@@ -131,92 +96,68 @@ public class CommandPromptWidget(string text, int cursorPos) : Renderable
                 }
 
                 yield return new Token(start, i - start, TokenKind.QuotedValue);
-                expectingCommand = false;
-                expectingKeyOrFlag = true;
                 continue;
             }
 
-            if (IsIdentPart(c))
+            // highlight value in key=value
+            if (input[i] == '=')
             {
-                var start = i;
+                // apply default to =
+                yield return new Token(i, 1, TokenKind.Default);
                 i++;
-                while (i < input.Length && IsIdentPart(input[i]))
+
+                if (i >= input.Length)
                 {
-                    i++;
+                    break;
                 }
 
-                if (expectingCommand)
+                if (input[i] == '"')
                 {
-                    yield return new Token(start, i - start, TokenKind.Command);
-                    expectingCommand = false;
-                    expectingKeyOrFlag = true;
                     continue;
                 }
 
-                if (i < input.Length && input[i] == '=')
+                // unquoted value until whitespace or &&
+                var vStart = i;
+                while (i < input.Length)
                 {
-                    yield return new Token(start, i - start, TokenKind.Key);
-                    yield return new Token(i, 1, TokenKind.Equals);
-                    i++;
-
-                    if (i < input.Length && input[i] != '"' && !char.IsWhiteSpace(input[i]))
+                    if (char.IsWhiteSpace(input[i]))
                     {
-                        var vStart = i;
-                        while (i < input.Length)
-                        {
-                            if (char.IsWhiteSpace(input[i]))
-                            {
-                                break;
-                            }
-
-                            if (input[i] == '&' && i + 1 < input.Length && input[i + 1] == '&')
-                            {
-                                break;
-                            }
-
-                            i++;
-                        }
-
-                        if (i > vStart)
-                        {
-                            yield return new Token(vStart, i - vStart, TokenKind.Value);
-                        }
+                        break;
                     }
 
-                    expectingKeyOrFlag = true;
-                    continue;
+                    if (input[i] == '&' && i + 1 < input.Length && input[i + 1] == '&')
+                    {
+                        break;
+                    }
+
+                    i++;
                 }
 
-                // flag (no =) or key in progress
-                if (expectingKeyOrFlag)
+                if (i > vStart)
                 {
-                    yield return new Token(start, i - start, TokenKind.Key);
-                    continue;
+                    yield return new Token(vStart, i - vStart, TokenKind.Value);
                 }
 
-                yield return new Token(start, i - start, TokenKind.Value);
                 continue;
             }
 
-            if (c == '=')
+            // everything else default
+            var startDefault = i;
+            while (i < input.Length && input[i] != '"' && input[i] != '=')
             {
-                yield return new Token(i, 1, TokenKind.Equals);
                 i++;
-                continue;
             }
 
-            yield return new Token(i, 1, TokenKind.Value);
-            i++;
+            if (i > startDefault)
+            {
+                yield return new Token(startDefault, i - startDefault, TokenKind.Default);
+            }
         }
     }
 
     private enum TokenKind
     {
-        Whitespace,
-        AndAnd,
-        Command,
-        Key,
-        Equals,
+        Default,
         Value,
         QuotedValue,
         Cursor,
