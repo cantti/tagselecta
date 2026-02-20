0d6c22818b65ec17b7d651c4f13ef7e96488e473
cantti <cantti@outlook.com>
2026-02-20 21:55:46 +0100

    url update

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://cantti.github.io/tagselecta',
+  url: 'https://cantti.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/tagselecta',
