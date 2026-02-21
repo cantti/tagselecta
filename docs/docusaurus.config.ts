5f56324410e9f53d2a55176f819b4b4ee1bd9571
cantti <cantti@outlook.com>
2026-02-21 19:52:14 +0100

    icon

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,13 +1,13 @@
-import { themes as prismThemes } from 'prism-react-renderer';
-import type { Config } from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'TagSelecta',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'favicon.svg',
+  title: "TagSelecta",
+  tagline: "Dinosaurs are cool",
+  favicon: "favicon.ico",
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -15,36 +15,36 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://cantti.github.io',
+  url: "https://cantti.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/tagselecta',
+  baseUrl: "/tagselecta",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'cantti', // Usually your GitHub org/user name.
-  projectName: 'tagselecta', // Usually your repo name.
+  organizationName: "cantti", // Usually your GitHub org/user name.
+  projectName: "tagselecta", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: "throw",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          routeBasePath: '/',
-          sidebarPath: './sidebars.ts',
+          routeBasePath: "/",
+          sidebarPath: "./sidebars.ts",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -52,16 +52,16 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: "img/docusaurus-social-card.jpg",
     colorMode: {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: 'TagSelecta',
-      // logo: {
-      //   alt: 'My Site Logo',
-      //   src: 'img/logo.svg',
-      // },
+      title: "TagSelecta",
+      logo: {
+        alt: "My Site Logo",
+        src: "/android-chrome-192x192.png",
+      },
       items: [
         // {
         //   type: 'docSidebar',
@@ -70,9 +70,9 @@ const config: Config = {
         //   label: 'Docs',
         // },
         {
-          href: 'https://github.com/cantti/tagselecta',
-          label: 'GitHub',
-          position: 'left',
+          href: "https://github.com/cantti/tagselecta",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
