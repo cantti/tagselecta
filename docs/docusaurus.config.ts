7d703e70090e8891eabe530fb86b4901df0d06e9
cantti <cantti@outlook.com>
2026-02-21 13:33:15 +0100

    docs

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'TagSelecta',
   tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  favicon: 'favicon.svg',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
