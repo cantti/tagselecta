89a82a29eecc0bc1a39b41a7c5685d13ae5872a1
cantti <cantti@outlook.com>
2026-03-30 00:55:42 +0200

    music brainz section improvements

--- TagSelecta.App/Config/MusicBrainzSection.cs
+++ TagSelecta.App/Config/MusicBrainzSection.cs
@@ -6,15 +6,16 @@ public class MusicBrainzSection
         new(StringComparer.OrdinalIgnoreCase)
         {
             ["album"] = "{{ release.title }}",
-            ["albumartist"] = "{{ release.artistcredit[0].name }}",
+            ["albumartist"] = "{{ release.artistcredit | array.map 'name' | joined }}",
             ["artist"] =
-                "{{ if tracks[index].artistcredit && tracks[index].artistcredit.size > 0; tracks[index].artistcredit | array.map 'name' | joined; else; release.artistcredit | array.map 'name' | joined; end }}",
-            ["catalognumber"] = "{{ release.labelinfo[0].catalognumber }}",
+                "{{ if tracks[index].artistcredit.size > 0; tracks[index].artistcredit | array.map 'name' | joined; else; release.artistcredit | array.map 'name' | joined; end }}",
+            ["catalognumber"] =
+                "{{ release.labelinfo | array.map 'label' | array.map 'catalognumber' | joined }}",
             ["comment"] = "{{ release.disambiguation }}",
             ["date"] = "{{ release.date }}",
-            ["genre"] = "{{ release.releasegroup.genres | array.map 'name' | joined }}",
-            ["label"] = "{{ release.labelinfo[0].label.name }}",
-            ["title"] = "{{ tracks[index].title }}",
+            ["genre"] = "{{ release.releasegroup?.genres | array.map 'name' | joined }}",
+            ["label"] = "{{ release.labelinfo | array.map 'label' | array.map 'name' | joined }}",
+            ["title"] = "{{ tracks[index]?.title }}",
             ["track"] = "{{ index + 1 }}",
             ["tracktotal"] = "{{ tracks.size }}",
             ["musicbrainz_release_id"] = "{{ release.id }}",
