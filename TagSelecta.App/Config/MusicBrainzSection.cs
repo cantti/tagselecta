039b0208d46731ee13949e5dba995540788888e1
cantti <cantti@outlook.com>
2026-03-30 00:20:18 +0200

    refacroing

--- TagSelecta.App/Config/MusicBrainzSection.cs
+++ TagSelecta.App/Config/MusicBrainzSection.cs
@@ -9,14 +9,14 @@ public class MusicBrainzSection
             ["albumartist"] = "{{ release.artistcredit[0].name }}",
             ["artist"] =
                 "{{ if tracks[index].artistcredit && tracks[index].artistcredit.size > 0; tracks[index].artistcredit | array.map 'name' | joined; else; release.artistcredit | array.map 'name' | joined; end }}",
+            ["catalognumber"] = "{{ release.labelinfo[0].catalognumber }}",
+            ["comment"] = "{{ release.disambiguation }}",
+            ["date"] = "{{ release.date }}",
+            ["genre"] = "{{ release.releasegroup.genres | array.map 'name' | joined }}",
+            ["label"] = "{{ release.labelinfo[0].label.name }}",
             ["title"] = "{{ tracks[index].title }}",
             ["track"] = "{{ index + 1 }}",
             ["tracktotal"] = "{{ tracks.size }}",
-            ["genre"] = "{{ release.releasegroup.genres | array.map 'name' | joined }}",
-            ["date"] = "{{ release.date }}",
-            ["label"] = "{{ release.labelinfo[0].label.name }}",
-            ["catalognumber"] = "{{ release.labelinfo[0].catalognumber }}",
-            ["comment"] = "{{ release.disambiguation }}",
             ["musicbrainz_release_id"] = "{{ release.id }}",
         };
 }
