8ba297b13dee7afee4adc52e0739d4b08995e60f
cantti <cantti@outlook.com>
2026-04-16 22:06:43 +0200

    Enable tree view in GeneralSection configuration by default.

--- TagSelecta.App/Config/GeneralSection.cs
+++ TagSelecta.App/Config/GeneralSection.cs
@@ -6,5 +6,5 @@ public class GeneralSection
     public bool Debug { get; set; }
     public double FileListRatio { get; set; } = 0.3;
     public bool AutoCompletionEnabled { get; set; } = true;
-    public bool TreeEnabled { get; set; } = false;
+    public bool TreeEnabled { get; set; } = true;
 }
