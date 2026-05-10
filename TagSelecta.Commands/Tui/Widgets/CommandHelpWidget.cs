1d48b2b0ff75d7a50cf292bd2a34f12c1de7cbbb
cantti <cantti@outlook.com>
2026-05-10 23:20:37 +0200

    update CommandHelpWidget.cs

--- TagSelecta.Commands/Tui/Widgets/CommandHelpWidget.cs
+++ TagSelecta.Commands/Tui/Widgets/CommandHelpWidget.cs
@@ -18,33 +18,33 @@ public class CommandHelpWidget : Renderable
 
     private static IRenderable CommandHelp()
     {
-        var valueColor = Color.Yellow;
         var commandColor = Color.Blue;
         var commands = new List<(string Command, string Action)>
         {
             ($"[bold {commandColor}]:selectdir[/]", "Select all files in dir"),
             ($"[bold {commandColor}]:write[/]", "Save changes"),
             (
-                $"[bold {commandColor}]:edit[/] artist=[{valueColor}]Bach[/] title=[{valueColor}]\"The Goldberg Variations\"[/] key=[{valueColor}]url[/] value=[{valueColor}]https://example.com[/]",
+                $"[bold {commandColor}]:edit[/] artist=Bach title=\"The Goldberg Variations\" url=https://example.com",
                 "Edit tags"
             ),
             ($"[bold {commandColor}]:autotrack[/]", "Auto track number"),
             ($"[bold {commandColor}]:split[/]", "Split artists"),
             ($"[bold {commandColor}]:titlecase[/]", "Title case conversion"),
             (
-                $"[bold {commandColor}]:move[/] template=[{valueColor}]\"../{{ year }} - {{ album }}/{{ filename }}.{{ext}}\"[/]",
+                $"[bold {commandColor}]:move[/] template=\"../{{{{ year }}}} - {{{{ album }}}}/{{{{ filename }}}}.{{{{ext}}}}\"",
                 "Move file using template"
             ),
             ($"[bold {commandColor}]:extractpicture[/]", "Extract picture"),
+            ($"[bold {commandColor}]:clearexcept[/] artist album title", "Clear all fields except"),
             (
-                $"[bold {commandColor}]:discogs[/] release=[{valueColor}]\"https://www.discogs.com/release/...\"[/]",
+                $"[bold {commandColor}]:discogs[/] release=\"https://www.discogs.com/release/...\"",
                 "Discogs"
             ),
             (
-                $"[bold {commandColor}]:musicbrainz[/] release=[{valueColor}]\"https://musicbrainz.org/release/...\"[/]",
+                $"[bold {commandColor}]:musicbrainz[/] release=\"https://musicbrainz.org/release/...\"",
                 "MusicBrainz"
             ),
-            ($"[bold {commandColor}]:macro[/] [{valueColor}]<name>[/]", "Execute macro"),
+            ($"[bold {commandColor}]:macro[/] <name>", "Execute macro <name>"),
         };
         var result = new Grid();
         result.AddColumns(2);
